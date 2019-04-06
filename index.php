659a9023805e99ef9771fe8d524a9edd2948c55d
Dan Chamorro <dan@danchamorro.com>
2019-04-05 23:10:42 -0400

    Varibales and Constants

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 
-// echo 'hello, ninjas';
+$name = "Dan";
+$age = 30;
+define("NAME", "Joe");
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>My first PHP File</title>
 </head>
+
 <body>
-    <h1> <?php echo 'hello, ninjas'; ?></h1>
+    <h1> User Profile Page</h1>
+    <div><?php echo $name; ?></div>
+    <div><?php echo NAME; ?></div>
 
 </body>
+
 </html>
